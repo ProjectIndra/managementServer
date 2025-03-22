df3d3dc669a853da04e4c041a07421b74d73c784
SUBROTOKOIRY <koirysubroto1103@gmail.com>
2025-03-22 18:08:14 +0530

    completed the flow of creating a vm

--- provider_controllers/vm_crud.py
+++ provider_controllers/vm_crud.py
@@ -1,6 +1,9 @@
 from flask import request, jsonify
 import requests
+import time
 
+# internal imports
+from cli_controllers import wg
 from provider_controllers import telemetry
 
 def vm_creation():
@@ -15,72 +18,65 @@ def vm_creation():
 
 def helper_vm_creation(request):
     """
-    This is a helper function responsible for creating new a VM
+    This is a helper function responsible for creating a new VM
     """
     try:
         data = request.get_json()
-        vm_name = data.get('vm_name', 'new-vm')
-        vcpus = data.get('vcpus')
-        ram = data.get('ram')
-        storage= data.get('storage')
-        vm_image = data.get('vm_image')
-        provider_id = data.get('provider_id')
-        client_id = data.get('client_id')
-        remarks = data.get('remarks')
-
-        print(data)
-
-        # Hardcoded provider URL
         provider_url = 'https://pet-muskox-honestly.ngrok-free.app'
+        
+        vm_name = data.get('vm_name', 'new-vm')
+        vm_data = {
+            "name": vm_name,
+            "vcpus": data.get('vcpus'),
+            "memory": data.get('ram'),
+            "storage": data.get('storage'),
+            "image": data.get('vm_image'),
+            "qvm_path": "/var/lib/libvirt/images/avinash.qcow2",
+        }
 
-        # networkList_response=requests.get(f'{provider_url}/network/list')
-        # networkList_response_json=networkList_response.json()
-        # print(networkList_response.json())
+        network_list = get_network_list(provider_url)
+        if "default" not in network_list.get('active_networks', []) and "default" not in network_list.get('inactive_networks', []):
+            create_default_network(provider_url)
+        elif "default" in network_list.get('inactive_networks', []):
+            activate_default_network(provider_url)
+        
+        vm_response = create_vm(provider_url, vm_data)
+        vm_response_json = vm_response.json()
 
-        # if "default" not in networkList_response_json.get('networks'):
-        #     print("hii")
-        #     network_data = {
-        #         "name": "default",
-        #         "bridgeName": "virbr1",
-        #         "forwardMode": "nat",
-        #         "ipAddress": "192.168.122.1",
-        #         "ipRangeStart": "192.168.122.100",
-        #         "ipRangeEnd": "192.168.122.200",
-        #         "netMask": "255.255.255.0"
-        #     }
+        
+        wireguard_response = wg.setup_wireguard(
+            provider_url, data
+        )
 
-        #     # need to check this if this route is working or not
-        #     network_create_response = requests.post(f"{provider_url}/network/create", json=network_data)
-
-        #     print(network_create_response.status_code)
-
-        # sending request to create VM
-
-        # vm_data={
-        #     "name":vm_name,
-        #     "vcpus":vcpus,
-        #     "memory":ram,
-        #     "storage":storage,
-        #     "image":vm_image,
-        #     "qvm_path":"/var/lib/libvirt/images/avinash.qcow2"
-        # }
-
-        # vm_create_response=requests.post(f"{provider_url}/vm/create_qvm",json=vm_data)
-        # vm_create_response_json=vm_create_response.json()
-
-        # print(vm_create_response_json)
-
-        # check the dhcp ips and if it's there then only setup the wireguard configuration
-
-        dhcp_ip_lists_response=requests.post(f"{provider_url}/vm/ipaddresses", json={"vm_name":vm_name})
-        print(dhcp_ip_lists_response.json())
+        return jsonify({
+            "message": "Congrats, VM is successfully created",
+            "vm_public_key": wireguard_response.get('public_key')
+        }), 200
     except requests.RequestException as e:
-        print(f"Proxy error: {e}")
         return jsonify({"error": "Failed to reach provider", "details": str(e)}), 500
 
 
+def get_network_list(provider_url):
+    response = requests.get(f'{provider_url}/network/list')
+    return response.json()
 
-    return jsonify({"message": "Congrats VM is successfully created"}), 200
+def create_default_network(provider_url):
+    network_data = {
+        "name": "default",
+        "bridgeName": "virbr1",
+        "forwardMode": "nat",
+        "ipAddress": "192.168.122.1",
+        "ipRangeStart": "192.168.122.100",
+        "ipRangeEnd": "192.168.122.200",
+        "netMask": "255.255.255.0"
+    }
+    return requests.post(f"{provider_url}/network/create", json=network_data)
+
+def activate_default_network(provider_url):
+    return requests.post(f"{provider_url}/network/activate", json={"name": "default"})
+
+def create_vm(provider_url, vm_data):
+    return requests.post(f"{provider_url}/vm/create_qvm", json=vm_data)
 
 
 
